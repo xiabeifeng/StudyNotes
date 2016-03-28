5cd25ecfd217a32cb412292c010dc1a835769b0b
xiabeifeng <840679824@qq.com>
2016-03-28 14:43:37 +0800

    Update PHP空对象.php

--- PHP/PHP空对象.php
+++ PHP/PHP空对象.php
@@ -8,8 +8,27 @@ $obj->name = 'xiabeifeng';
 $obj->sex  = '男';
 var_dump($obj);
 
-$newObj = $obj;         // 默认传引用(节省内存空间)
-$newObj = clone $obj;   // 传值
+$newObj1 = $obj;         // 默认传引用(节省内存空间)
+echo '<pre>';
+var_dump($obj);
+var_dump($newObj1);
+echo '</pre>';
+$obj->age = 24;
+echo '<pre>';
+var_dump($obj);
+var_dump($newObj1);
+echo '</pre>';
+
+$newObj2 = clone $obj;   // 传值
+echo '<pre>';
+var_dump($obj);
+var_dump($newObj2);
+echo '</pre>';
+$obj->age = 25;
+echo '<pre>';
+var_dump($obj);
+var_dump($newObj2);
+echo '</pre>';
 
 $array = array('name'=>'xiabeifeng', 'sex'=>'男');
 $newArray = $array;     // 默认传值
