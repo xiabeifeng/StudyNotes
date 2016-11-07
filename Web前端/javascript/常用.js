6748f9284559d998bd194ad7ba63df96d1764d86
夏北风 <840679824@qq.com>
2016-11-07 14:37:45 +0800

    如果有上一个页面就跳转到上一个页面,如果没有就关闭窗口

--- Web前端/javascript/常用.js
+++ Web前端/javascript/常用.js
@@ -3,3 +3,9 @@ $('input:radio:checked').val();
 $('input[type="radio"]:checked').val();
 $('input[name="gender"]:checked').val();
 
+// 如果有上一个页面就跳转到上一个页面,如果没有就关闭窗口
+if (history.length > 0) {
+    history.back();
+} else {
+    window.close();
+}
