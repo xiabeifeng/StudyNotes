16079d0a01e03f19f99519a096ffe22fa8084244
夏北风 <840679824@qq.com>
2016-11-08 15:21:09 +0800

    WeixinJSBridge.call('closeWindow'); 关闭微信浏览器网页窗口

--- Web前端/javascript/常用.js
+++ Web前端/javascript/常用.js
@@ -4,8 +4,9 @@ $('input[type="radio"]:checked').val();
 $('input[name="gender"]:checked').val();
 
 // 如果有上一个页面就跳转到上一个页面,如果没有就关闭窗口
-if (history.length > 0) {
+if (history.length > 1) {
     history.back();
 } else {
     window.close();
+    // WeixinJSBridge.call('closeWindow'); // 微信浏览器专用
 }
